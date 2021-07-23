63b3c7ca2a39c4312d66fbc96691ed25537902b2
Bruno Kohn <brunokohn1993@gmail.com>
2021-07-23 10:35:46 -0300

    lógica dos selecionados implementada

--- scripts.js
+++ scripts.js
@@ -1,4 +1,49 @@
-function selectPlate() {
-    const element = document.querySelector(".plate-box");
-    element.classList.toggle("selected");
+let number = 0;
+
+function selectPlate(plate) {    
+    const element = document.querySelector('.plates .selected');
+
+    if(element !== null) {        
+        element.classList.remove("selected");
+        number--;
+    }
+
+    const clickedPlate = document.querySelector(plate);
+    clickedPlate.classList.add("selected");
+    sumNumber();
+    
 }
+
+function selectBeverage(beverage) {    
+    const element = document.querySelector('.beverages .selected');
+
+    if(element !== null) {        
+        element.classList.remove("selected");
+        number--;
+    }
+
+    const clickedBeverage = document.querySelector(beverage);
+    clickedBeverage.classList.add("selected");
+    sumNumber();
+}
+
+function selectDessert(dessert) {    
+    const element = document.querySelector('.desserts .selected');
+
+    if(element !== null) {        
+        element.classList.remove("selected");
+        number--;
+    }
+
+    const clickedDessert = document.querySelector(dessert);
+    clickedDessert.classList.add("selected");
+    sumNumber();
+}
+
+function sumNumber() {
+    number++;
+    console.log(number);
+}
+
+
+
